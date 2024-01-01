f77ac93d304b4d7c65bd3ac7cc9153960bb54880
estecore <estecore@yandex.ru>
2024-01-01 14:45:53 +0400

    add item to local storage

---
+++ js/app.js
@@ -0,0 +1,33 @@
+if (!localStorage.getItem('goods')) {
+    localStorage.setItem('goods', JSON.stringify([]));
+}
+
+let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+    keyboard: false});
+
+document.querySelector('button.add_new').addEventListener('click', function(e) {
+    let name = document.getElementById('good_name').value,
+        price = document.getElementById('good_price').value,
+        count = document.getElementById('good_count').value;
+
+    if (name && price && count) {
+        document.getElementById('good_name').value = '';
+        document.getElementById('good_price').value = '';
+        document.getElementById('good_count').value = '1';
+
+        let goods = JSON.parse(localStorage.getItem('goods'));
+        goods.push(['good_' + goods.length, name, price, count, 0, 0, 0]);
+        localStorage.setItem('goods', JSON.stringify(goods));
+
+        // update_goods()
+
+        myModal.hide();
+
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Ошибка',
+            text: 'Пожалуйста, заполните все поля',
+        })
+    }
+})
