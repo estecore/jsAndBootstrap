c0bb7498b70d64f97732370fcaa220be64ecaa19
estecore <estecore@yandex.ru>
2024-01-01 17:05:04 +0400

    function update interface

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ document.querySelector('button.add_new').addEventListener('click', function(e) {
         goods.push(['good_' + goods.length, name, price, count, 0, 0, 0]);
         localStorage.setItem('goods', JSON.stringify(goods));
 
-        // update_goods()
+        update_goods()
 
         myModal.hide();
 
@@ -31,3 +31,58 @@ document.querySelector('button.add_new').addEventListener('click', function(e) {
         })
     }
 })
+
+update_goods();
+
+function update_goods() {
+    let result_price = 0,
+        tbody = document.querySelector('.list');
+    tbody.innerHTML = '';
+    document.querySelector('.cart').innerHTML = '';
+    let goods = JSON.parse(localStorage.getItem('goods'));
+    if (goods.length) {
+        table1.hidden = false;
+        table2.hidden = false;
+
+        for (let i = 0; i < goods.length; i++) {
+            tbody.insertAdjacentHTML('beforeend', 
+            `
+            <tr class='align-middle'>
+                <td>${i+1}</td>
+                <td class='name'>${goods[i][1]}</td>
+                <td class='price'>${goods[i][2]}</td>
+                <td>${goods[i][3]}</td>
+                <td><button class='goods_delete btn btn-danger' data-delete='${goods[i][0]}'>&#10006;</button></td>
+                <td><button class='goods_delete btn btn-primary' data-goods='${goods[i][0]}'>&#10149;</button></td>
+            </tr>
+            `
+            )
+            if (goods[i][4] > 0) {
+                goods[i][6] = goods[i][4] * goods[i][2] - goods[i][4] * goods[i][2] * goods[i][5] * 0.01;
+                result_price += goods[i][6];
+                document.querySelector('.cart').insertAdjacentHTML('beforeend',
+                    `
+                <tr class='align-middle'>
+                    <td>${i+1}</td>
+                    <td class='price_name'>${goods[i][1]}</td>
+                    <td class='price_one'>${goods[i][2]}</td>
+                    <td class='price_count'>${goods[i][4]}</td>
+                    <td class='price_discount'><input data-goodid='${goods[i][0]}' type='text' value='${goods[i][5]}' min='0' max='100'></td>
+                    <td>${goods[i][6]}</td>
+                    <td><button class='goods_delete btn btn-danger' data-delete='${goods[i][0]}'>&#10006;</button></td>
+                </tr>
+                `
+            )
+            }
+        }
+
+        // userlist = new list('goods', options);
+
+    } else {
+        table1.hidden = true;
+        table2.hidden = true;
+    }
+
+    document.querySelector('.price_result').innerHTML = result_price + '&#8381;'
+
+};
