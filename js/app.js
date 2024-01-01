b0c43d52643d3c3f25065abaaea0aeb1bdc5e57d
estecore <estecore@yandex.ru>
2024-01-01 19:05:33 +0400

    sort function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
+// ============ sort elements
+
+table1.onclick = function(e) {
+    if (e.target.tagName != 'TH') {
+        return;
+    } else {
+        let th = e.target;
+        sortTable(th.cellIndex, th.dataset.type, 'table1');
+    }
+}
+
+table2.onclick = function(e) {
+    if (e.target.tagName != 'TH') {
+        return;
+    } else {
+        let th = e.target;
+        sortTable(th.cellIndex, th.dataset.type, 'table2');
+    }
+}
+
+function sortTable(colNum, type, id) {
+    let elem = document.getElementById(id);
+    let tbody = elem.querySelector('tbody');
+    let rowsArray = Array.from(tbody.rows);
+    let compare;
+    switch(type) {
+        case 'number':
+            compare = function(rowA, rowB) {
+                return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML
+            }
+            break;
+        case 'string':
+        compare = function(rowA, rowB) {
+            return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1
+        }
+        break;
+    }
+
+    rowsArray.sort(compare);
+    tbody.append(...rowsArray);
+}
+
+
+// ============ 
+
 if (!localStorage.getItem('goods')) {
     localStorage.setItem('goods', JSON.stringify([]));
 }
